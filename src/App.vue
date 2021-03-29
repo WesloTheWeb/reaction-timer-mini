8d74beb0d2f5c5bcf4d194cbe9a29dd0e89b7fb4
Wesley <wesleyadwebster@gmail.com>
2021-03-29 03:31:38 -0500

    Fleshed out components and added styling

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <template>
-  <h1>Reaction Timer mini </h1>
+  <h1>Reaction Timer mini</h1>
+  <button id="start-button" @click="start">Play</button>
+  <Block v-if="isPlaying" />
 </template>
 
 <script>
+import Block from "./components/Block.vue";
 
 export default {
-  name: 'App',
- 
-}
+  name: "App",
+  components: { Block },
+
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000; // gets us a random amount of miliseconds.
+      this.isPlaying = true;
+      console.log(this.delay);
+    },
+  },
+};
 </script>
 
 <style>
+body {
+  background-color: #a9a9a9;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,5 +40,26 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+
+button {
+  font-weight: bold;
+  font-size: 1.5rem;
+  padding: 1rem;
+  border-radius: 10px;
+}
+
+button:hover {
+  cursor: pointer;
+  filter: brightness(85%);
+}
+
+#start-button {
+  background-color: limegreen;
+  color: #fff;
+}
+
+#start-button:hover {
+  color: #000;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="block">click me</button>
+</template>
+
+<script>
+export default {
+  props: {
+    delay: Array,
+  },
+};
+</script>
+
+<style>
+.block {
+  display: block;
+  width: 10rem;
+  height: 10rem;
+  border-radius: 50%;
+  border: solid #000 5px;
+  background-color: crimson;
+  color: #fff;
+  text-align: center;
+  text-transform: uppercase;
+  padding: 2rem 0;
+  margin: 4rem auto;
+}
+</style>
